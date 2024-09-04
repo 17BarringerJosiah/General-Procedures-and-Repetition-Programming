f220b4ca9847f5c5d34f486a45b9ddc378d05a05
Josiah Barringer <59709224+17BarringerJosiah@users.noreply.github.com>
2024-09-04 13:49:05 -0400

    Add files via upload

---
+++ JB_Project3/JB_Project3/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmbServingTime = New System.Windows.Forms.ComboBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.txtNumPeople = New System.Windows.Forms.TextBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.txtNumVolunteers = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'cmbServingTime
+        '
+        Me.cmbServingTime.FormattingEnabled = True
+        Me.cmbServingTime.Items.AddRange(New Object() {"Breakfast", "Lunch", "Dinner"})
+        Me.cmbServingTime.Location = New System.Drawing.Point(130, 96)
+        Me.cmbServingTime.Name = "cmbServingTime"
+        Me.cmbServingTime.Size = New System.Drawing.Size(121, 21)
+        Me.cmbServingTime.TabIndex = 3
+        Me.cmbServingTime.Text = "Serving Options"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(153, 150)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 4
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'txtNumPeople
+        '
+        Me.txtNumPeople.Location = New System.Drawing.Point(63, 61)
+        Me.txtNumPeople.Name = "txtNumPeople"
+        Me.txtNumPeople.Size = New System.Drawing.Size(121, 20)
+        Me.txtNumPeople.TabIndex = 1
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(153, 179)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(153, 208)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 6
+        Me.btnClose.Text = "&Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'txtNumVolunteers
+        '
+        Me.txtNumVolunteers.Location = New System.Drawing.Point(190, 61)
+        Me.txtNumVolunteers.Name = "txtNumVolunteers"
+        Me.txtNumVolunteers.Size = New System.Drawing.Size(121, 20)
+        Me.txtNumVolunteers.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(78, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(92, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Number of People"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(187, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(109, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Number of Volunteers"
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.Location = New System.Drawing.Point(390, 61)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(282, 264)
+        Me.lstOutput.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtNumVolunteers)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.txtNumPeople)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.cmbServingTime)
+        Me.Name = "Form1"
+        Me.Text = "Soup Kitchen"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmbServingTime As ComboBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents txtNumPeople As TextBox
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents txtNumVolunteers As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lstOutput As ListBox
+End Class
---
+++ JB_Project3/JB_Project3/Form1.vb
@@ -0,0 +1,100 @@
+﻿' General Procedures and Repetition Programming Assignment
+' Purpose: Calculate the total cost of the meals being served based on the number of
+' people And the type of service.
+' Josiah Barringer
+
+' I will not use code that was never covered in class or in our textbook. 
+' If you do, you must be able to explain your code when the professor asks. 
+' Using code outside of the resources provided can be flagged and reported 
+' as an academic integrity violation if there is any suspicion of copying/cheating.
+
+Public Class Form1
+    ' Function to determine the cost per meal based on the serving time
+    Private Function GetMealCost(ByVal servingTime As String) As Decimal
+        Select Case servingTime
+            Case "Breakfast"
+                Return 10D
+            Case "Lunch"
+                Return 15D
+            Case "Dinner"
+                Return 20D
+            Case Else
+                Return 0D
+        End Select
+    End Function
+
+    ' Subroutine to display the volunteer names in the ListBox
+    Private Sub DisplayVolunteers(ByVal numVolunteers As Integer)
+        lstOutput.Items.Add("The following is a list of volunteers:")
+        lstOutput.Items.Add("_______________________________________")
+        For i As Integer = 1 To numVolunteers
+            Dim name As String = InputBox("Enter the name of volunteer #" & i.ToString(), "Volunteer Names")
+            lstOutput.Items.Add(name)
+        Next
+    End Sub
+
+    ' Calculate button click event
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        ' Clear previous output
+        lstOutput.Items.Clear()
+
+        ' Input validation
+        If txtNumPeople.Text = "" Then
+            MessageBox.Show("Please enter the number of people served.", "Input Error")
+            txtNumPeople.Focus()
+            Exit Sub
+        End If
+
+        If txtNumVolunteers.Text = "" Then
+            MessageBox.Show("Please enter the number of volunteers.", "Input Error")
+            txtNumVolunteers.Focus()
+            Exit Sub
+        End If
+
+        If cmbServingTime.SelectedIndex = -1 Then
+            MessageBox.Show("Please select a serving time.", "Input Error")
+            cmbServingTime.Focus()
+            Exit Sub
+        End If
+
+        ' Store inputs in variables
+        Dim numPeople As Integer = CInt(txtNumPeople.Text)
+        Dim numVolunteers As Integer = CInt(txtNumVolunteers.Text)
+        Dim servingTime As String = cmbServingTime.SelectedItem.ToString()
+        Dim costPerPerson As Decimal = GetMealCost(servingTime)
+        Dim totalCost As Decimal = costPerPerson * numPeople
+        Dim discount As Decimal = 0D
+        Dim discountApplied As Boolean = False
+
+        If numPeople > 10 Then
+            discount = totalCost * 0.1D
+            totalCost -= discount
+            discountApplied = True
+        End If
+
+        ' Display the results in the ListBox
+        lstOutput.Items.Add("You are volunteering to serve  " & servingTime & ".")
+        lstOutput.Items.Add("Your cost per person is $" & costPerPerson.ToString("F2"))
+        lstOutput.Items.Add("The total cost is $" & totalCost.ToString("F2"))
+        If discountApplied Then
+            lstOutput.Items.Add("A 10% discount has been applied.")
+        End If
+        lstOutput.Items.Add("You are bringing " & numVolunteers.ToString() & " to help serve.")
+
+        ' Display volunteer names
+        DisplayVolunteers(numVolunteers)
+    End Sub
+
+    ' Clear button click event
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtNumPeople.Clear()
+        txtNumVolunteers.Clear()
+        cmbServingTime.SelectedIndex = -1
+        lstOutput.Items.Clear()
+    End Sub
+
+    ' Close button click event
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+End Class
